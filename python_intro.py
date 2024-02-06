a5dbe3fa8bdca6876a855d9d0af48dd388a2b3c0
kadir-zeren <k4dzeren@gmail.com>
2024-02-07 01:19:27 +0300

    print

--- python_intro.py
+++ python_intro.py
@@ -1,1 +1,29 @@
 print("hello")
+print('tek tirnak')
+print("cift tirnak")
+print('''uc tirnak ''')
+print("I'm great")
+print('"hi" he sadi')
+print('''"Don't do it" said the manager''')
+print('''bu birinci satir
+bu ikinci satir 
+bu uc
+bu dort  ''')
+print(3.14 * 3)
+print('3.14' * 3)
+print(''''we should have enough time for our family"''')
+print('bir')
+print()
+print('uc')
+print('I\'m great')
+print("\"don't do it\" said the manager")
+print('bu benim cumlem \nbu ikinci satir')
+print('bu benim cumlem \tbu ikinci satir')
+print("la;dk;alskdadjadhajkaldla aljddd aldasd \
+      asdnjasndjsandjadjn\
+      adadasd")
+print("bu bir yorum satiridir") #yorum satiri
+print('kullanici ismi') # kullanici ismi yazdiriliyor
+x = 5
+print(5)
+print('hello')
